9730acd212bb42951fd8b5babe8ab37c905bcdab
yangmingzi <yangmingzi@wondersgroup.com>
2023-07-19 17:57:10 +0800

    项目初始化

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,18 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
+const resolve = (dir: string) => path.join(__dirname, dir)
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/myDemos/'
+  base: '/myDemos/',
+  resolve: {
+    alias: {
+      '@': resolve('src')
+    }
+  },
+  server: {
+    open: true
+  }
 })
