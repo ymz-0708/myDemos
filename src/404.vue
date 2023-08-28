6b3694560c15825756e45e80e1391162e5864d15
yangmingzi <yangmingzi@wondersgroup.com>
2023-08-28 16:15:48 +0800

    404页面、打包构建

--- src/404.vue
+++ src/404.vue
@@ -1,22 +1,26 @@
 <template>
     <div class="pg-404">
-        404
+        <div class="container">
+            <h1>404 NOT FOUND</h1>
+        </div>
         <el-button @click="goHome">HOME</el-button>
     </div>
 </template>
 
 <script setup lang="ts">
 import { ref, reactive } from "vue";
-import { useRouter } from 'vue-router'
+import { useRouter } from "vue-router";
 
 const router = useRouter();
 // 回到主页
 const goHome = () => {
-    router.push('/')
+    router.push("/");
 };
 </script>
 
 <style lang="scss" scoped>
 .pg-404 {
+    height: 100vh;
 }
+
 </style>
