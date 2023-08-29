3085a0b6fc9ee3b23f11cd0167bb54daf1db203c
yangmingzi <yangmingzi@wondersgroup.com>
2023-08-29 15:18:17 +0800

    merge

--- src/components/cssDemos.vue
+++ src/components/cssDemos.vue
@@ -1,21 +1,27 @@
 <template>
     <div class="app">
         <div class="box">
-            <span class="content"> 主页 </span>
+            <el-button @click="btnClick"> 点击一下 </el-button>
+            {{ result }}
         </div>
     </div>
 </template>
 
 <script setup lang="ts">
 import { ref, reactive } from "vue";
-
+import http from "@utils/http";
+const result = ref('')
+const btnClick = () => {
+    http.get('/api/test').then((res: any) => {
+        result.value = res
+    })
+}
 </script>
 
 <style lang="scss" scoped>
 .app {
     .box {
         max-width: 300px;
-        background-color: antiquewhite;
         text-align: center;
         .content {
             display: inline-block;
